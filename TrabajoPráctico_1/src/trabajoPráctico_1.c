fe3a032585d12c1fc490771678df0d084914e313
Lorena-90 <xloli.etf.adtrx@gmail.com>
2020-09-11 18:29:24 -0300

    Creé la variable para el menú y agregué comentarios

--- TrabajoPráctico_1/src/trabajoPráctico_1.c
+++ TrabajoPráctico_1/src/trabajoPráctico_1.c
@@ -13,17 +13,29 @@
 
 int main(void) {
 
+
+	int opción; // Variable del menú
+
+	// Variables para el ingreso de datos
+	float número1;
+	float número2;
+
+   // Variables para iniciar las operaciones
 	float suma;
 	float resta;
 	float división;
 	float multiplicación;
 	float factorial;
 
+	// Variables para capturar el resultado que devuelve la función
+
 	float resultadoSuma;
 	float resultadoResta;
 	float resultadoDivisión;
 	float resultadoMultiplicación;
 	float resultadoFactorial;
+
+
 
 
 
