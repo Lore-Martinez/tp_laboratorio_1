1c877294dc50e164237dd66d24323b4abddce57b
Lorena-90 <xloli.etf.adtrx@gmail.com>
2020-09-11 19:59:01 -0300

    Incluí un menú y creé una variable de forma provisoria

--- TrabajoPráctico_1/src/trabajoPráctico_1.c
+++ TrabajoPráctico_1/src/trabajoPráctico_1.c
@@ -36,6 +36,32 @@ int main(void) {
 	float resultadoFactorial;
 
 
+	do
+	    {
+	        printf("1. Cargar\n2. Mostrar\n3. Eliminar\n4.Salir\n");
+	        printf("Elija una opcion: ");
+	        scanf("%d", &opción);
+	        switch(opción)
+	        {
+	            case 1:
+	                printf("\nEstoy cargando...\n");
+	            break;
+	            case 2:
+	                printf("Estoy mostrando...\n");
+	            break;
+	            case 3:
+	                printf("\nEstoy eliminando...\n");
+	            break;
+	            case 4:
+	                printf("\nGracias por usar el programa...\n");
+	            break;
+
+	        }
+
+
+
+
+	    }while(opción!=4);
 
 
 
@@ -54,6 +80,28 @@ int main(void) {
 
 
 
-	/* prints !!!Hello World!!! */
+
+
+
+
+
+
+
+
+
 	return EXIT_SUCCESS;
 }
+
+// Funciones para mi calculadora
+
+int Sumar(float número1, float número2) // prototipo de la función sumar
+
+{
+	float resultado;
+
+	resultado = número1 + número2;
+
+	return resultado;
+}
+
+
